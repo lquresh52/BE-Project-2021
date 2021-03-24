2a8c83f39ece0e7fe9c665fdeaa9ab9e63ab66cd
Bharat <choudharybharat9988@gmail.com>
2021-03-24 16:18:46 +0530

    Result Page,History Page,one time test allow

--- studentDashboard/urls.py
+++ studentDashboard/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('studentTest/<str:subject_id>/',views.studentTest,name="studentTest"),
     path('historyPage',views.historyPage,name='historyPage'),
-    path('result-page/<int:score>/<int:tot_ques>/<int:history_id>/',views.result_page,name='result-page'),
+    path('result-page/<int:score>/<int:history_id>/<int:tot_ques>/',views.result_page,name='result-page'),
     # path('timer/', views.timer_update, name='timer-update'),
 ]
